e0bf33acc0477094286a42573223c9e445ddec3c
Colin Kinloch <colinmkinloch@gmail.com>
2014-05-06 12:13:05 +0100

    normalisation

--- sql/create_table_attending.sql
+++ sql/create_table_attending.sql
@@ -1,7 +1,7 @@
 -- Attending Table
 CREATE TABLE IF NOT EXISTS attending(
+	attendid INTEGER PRIMARY KEY,
 	member INTEGER, -- FOREIGN member.uid
 	event INTEGER, -- FOREIGN event.eid
-	rsvp BOOLEAN DEFAULT false NOT NULL,
-	PRIMARY KEY(member, event)
+	rsvp BOOLEAN DEFAULT false NOT NULL
 );
--- sql/create_table_promote.sql
+++ sql/create_table_promote.sql
@@ -1,7 +1,7 @@
 -- Promote Table
 CREATE TABLE IF NOT EXISTS promote(
+	promoid INTEGER PRIMARY KEY,
 	promotion INTEGER, -- FOREIGN promotion.pid
 	event INTEGER, -- FOREIGN event.eid
-	cutoff TIMESTAMP,
-	PRIMARY KEY(promotion, event)
+	cutoff TIMESTAMP
 );
